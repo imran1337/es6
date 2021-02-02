da975525159d2e33c2e705e9ae3c73f6b175b01a
Coding Bird <Codingbird@yahoo.com>
2021-02-03 05:23:39 +1200

    closure basics concept learned

---
+++ M23_es6_some_advanced_topics/closure.js
@@ -0,0 +1,31 @@
+function stopWatch() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const clock1 = stopWatch()
+const clock2 = stopWatch()
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log('clock 2 start');
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
