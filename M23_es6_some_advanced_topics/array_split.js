66bfcc9069023c4c60faf47a41925c715a6ad2aa
Coding Bird <Codingbird@yahoo.com>
2021-02-03 05:37:55 +1200

    slice,splice,join learned

---
+++ M23_es6_some_advanced_topics/array_split.js
@@ -0,0 +1,11 @@
+const numbers = [1,2,3,4,5,6,7,8,9]
+const part = numbers.slice(1,5)
+console.log(part);
+
+const removed = numbers.splice(1,5,77,77,88,99)
+console.log("removed:",removed);
+
+console.log("mainArr:",numbers);
+
+const together = numbers.join(" amar ")
+console.log(together);
